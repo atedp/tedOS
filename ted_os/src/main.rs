6744cf032f0b9d477fe85d168840efb846122724
atedp <atedplummer@gmail.com>
2018-06-22 13:52:57 -0400

    adding println to panic messaging

--- ted_os/src/main.rs
+++ ted_os/src/main.rs
@@ -8,16 +8,17 @@ extern crate volatile;
 extern crate spin;
 
 #[macro_use]
+mod vga_buffer;
+
+#[macro_use]
 extern crate lazy_static;
 
 #[panic_implementation]
 #[no_mangle]
-pub fn panic(_info: &PanicInfo) -> ! {
+pub fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
-
-#[macro_use]
-mod vga_buffer;
 
 //static HELLO: &[u8] = b"Hello World!";
 
@@ -25,6 +26,7 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
 
     println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop{}
 }
