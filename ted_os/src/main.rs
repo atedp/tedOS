f920770d1f11b2c57ac4825698ad988f2ee7e6ce
atedp <atedplummer@gmail.com>
2018-06-22 16:00:42 -0400

    added exit_qemu

--- ted_os/src/main.rs
+++ ted_os/src/main.rs
@@ -8,6 +8,7 @@ use core::panic::PanicInfo;
 extern crate volatile;
 extern crate spin;
 extern crate uart_16550;
+extern crate x86_64;
 
 #[cfg(test)]
 extern crate std;
@@ -32,12 +33,21 @@ pub fn panic(info: &PanicInfo) -> ! {
     loop{}
 }
 
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(0);
+}
+
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 
     println!("Hello World{}", "!");
     serial_println!("Hello Host{}", "!");
+
+    unsafe { exit_qemu(); }
 
     loop{}
 }
---
+++ ted_os/target/rls/debug/deps/ted_os-6e7420ec7c87fce7.d
@@ -0,0 +1,7 @@
+/home/ted/Repos/tedOS/ted_os/target/rls/debug/deps/ted_os-6e7420ec7c87fce7.rmeta: src/main.rs src/vga_buffer.rs src/serial.rs
+
+/home/ted/Repos/tedOS/ted_os/target/rls/debug/deps/ted_os-6e7420ec7c87fce7.d: src/main.rs src/vga_buffer.rs src/serial.rs
+
+src/main.rs:
+src/vga_buffer.rs:
+src/serial.rs:
