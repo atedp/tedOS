ab347c039f1924ef7c2ae66a5a9a887562877f2b
atedp <atedplummer@gmail.com>
2018-06-26 20:58:04 -0400

    implement IDT.load() w/ lazy_static

--- ted_os/src/main.rs
+++ ted_os/src/main.rs
@@ -10,9 +10,20 @@ extern crate ted_os;
 
 extern crate x86_64;
 use x86_64::structures::idt::{Idt, ExceptionStackFrame};
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref IDT: Idt = {
+        let mut idt = Idt::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
 pub fn init_idt() {
-    let mut idt = Idt::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
@@ -29,6 +40,12 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    init_idt();
+
+    //invoke a breakpoint exception 
+    x86_64::instructions::int3();
+
+    println!("It did not crash!{}", "");
     loop{}
 }
 
